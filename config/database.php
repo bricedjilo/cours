e35889b34acb827a4eddce949f34a6992a2c95b6
Brice Djilo <brice.djilo@endurance.com>
2020-06-06 10:23:49 -0600

    db

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DB_URL = getenv("CLEARDB_DATABASE_URL");
+
 return [
 
     /*
@@ -45,18 +47,18 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'url' => $DB_URL, #env('DATABASE_URL'),
+            'host' => $DB_URL['host'], #env('DB_HOST', '127.0.0.1'),
+            'port' => '3306', #env('DB_PORT', '3306'),
+            'database' => substr($DB_URL["path"], 1), #env('DB_DATABASE', 'forge'),
+            'username' => $DB_URL['user'], #env('DB_USERNAME', 'forge'),
+            'password' => $DB_URL['pass'], #env('DB_PASSWORD', ''),
+            'unix_socket' => '', #env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => false,
+            'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
