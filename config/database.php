3fea12ac966b2c0fd7b5b2214b192749b264b9d9
Brice Djilo <brice.djilo@endurance.com>
2020-06-06 10:50:59 -0600

    db

--- config/database.php
+++ config/database.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Str;
 
-$DB_URL = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$DB_URL = (getenv("CLEARDB_DATABASE_URL"));
 
-var_dump($DB_URL['host']);
+var_dump($DB_URL);
 
 return [
 
